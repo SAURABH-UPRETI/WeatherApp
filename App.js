44c619983bd42f11d7daf30ca4dd928cc381a016
Saurabh Upreti <83522982+SAURABH-UPRETI@users.noreply.github.com>
2021-09-02 12:54:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const https = require("https");
+const app = express();
+
+app.get("/", function (req, res) {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=9397b39b5d87fdf506fa853892bcae93&units=metric";
+  https.get(url, function (response) {
+    console.log(response.statusCode);
+
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      res.send("the temparature of dealhi is " + temp + "digree celcious");
+    });
+  });
+});
+
+app.listen(3000, function(){
+    console.log("server started on port 3000");
+})
+ 
